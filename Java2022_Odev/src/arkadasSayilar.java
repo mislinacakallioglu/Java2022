4a03efb1f13ce7099ecdf277c2a761633d1e3c90
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2022-09-27 17:25:10 +0300

    commit

---
+++ Java2022_Odev/src/arkadasSayilar.java
@@ -0,0 +1,23 @@
+public class arkadasSayilar {
+    public static void main(String[] args) {
+
+        int sayi1=220,sayi2=284,toplam1=0,toplam2=0;
+
+        for (int i=1;i<sayi1;i++)
+        {
+            if (sayi1 %i==0)
+                toplam1=toplam1+i;
+        }
+
+        for (int i=1;i<sayi2;i++)
+        {
+            if (sayi2%i==0)
+                toplam2=toplam2+i;
+        }
+
+        if (sayi1==toplam2&& sayi2==toplam1)
+            System.out.println("Bu iki sayi arkadastir. ");
+        else
+            System.out.println("Bu iki sayi arkadas degildir. ");
+    }
+}
