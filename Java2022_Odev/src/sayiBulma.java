206b54f7750e52926ed9065f4c68d02090cd60fe
Mislina Çakallıoğlu <93317917+mislinacakallioglu@users.noreply.github.com>
2022-09-28 20:48:43 +0300

    added

---
+++ Java2022_Odev/src/sayiBulma.java
@@ -0,0 +1,27 @@
+public class sayiBulma {
+    public static void main(String[] args) {
+
+        int [] sayilar = new int[]{1,2,5,7,9,0};
+        int aranacak = 9;
+        boolean varMi=false;
+
+        for (int sayi : sayilar)
+        {
+            if (sayi == aranacak)
+            {
+                varMi = true;
+                break;
+            }
+        }
+
+        if (varMi)
+        {
+            System.out.println("Sayi mevcuttur ");
+        }
+        else
+        {
+            System.out.println("Sayi mevcut degildir ");
+        }
+
+    }
+}
